4d212f56bee92aea12bbf354fa279432c089a846
Stephen Crespo <screspo988@gmail.com>
2016-03-31 18:51:07 -0400

    Add size to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317203328) do
+ActiveRecord::Schema.define(version: 20160331224306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160317203328) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.string   "size"
   end
 
   create_table "rental_locations", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 
+lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 products = Product.create([
-                          {name: "Castle Bounce", price: 200, description: "13'W x 13'L x 16'H"},
-                          {name: "Single Lane Slide", price: 400, description: "13'W x 24'W x 18'H Can be used wet or dry."},
-                          {name: "3 in 1 Bounce/Slide Combo", price: 300, description: "13'W x 26'L x 16'H"},
-                          {name: "Obstacle Course", price: 600, description: "11'W x 65'L x 16'H"}])
+                          {name: "Castle Bounce", price: 200, size: "13'W x 13'L x 16'H", description: lorem_ipsum},
+                          {name: "Single Lane Slide", price: 400, size: "13'W x 24'W x 18'H", description: lorem_ipsum},
+                          {name: "3 in 1 Bounce/Slide Combo", price: 300, size: "13'W x 26'L x 16'H", description: lorem_ipsum},
+                          {name: "Obstacle Course", price: 600, size: "11'W x 65'L x 16'H", description: lorem_ipsum}])
