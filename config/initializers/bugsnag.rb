88e47142d25ca2a66597d3ec30fc520a84ea3d5b
Stephen Crespo <screspo988@gmail.com>
2016-04-21 19:54:59 -0400

    bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,4 @@
 Bugsnag.configure do |config|
-  config.api_key = "eb8ede4434b59d11b0fcd60f0ee6d790"
-  config.notify_release_stages = ["alpha", "production"]
+  config.api_key = "13f1b5185f20416b48153b62c46ae325"
+  #config.notify_release_stages = ["alpha", "production"]
 end
