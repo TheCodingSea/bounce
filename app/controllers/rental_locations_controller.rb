f1f534f5cc874e50d2487fa780d6e8c2d20adb24
Stephen Crespo <screspo988@gmail.com>
2016-04-21 18:32:55 -0400

    Fixed rental form

--- app/controllers/rental_locations_controller.rb
+++ app/controllers/rental_locations_controller.rb
@@ -69,6 +69,7 @@ class RentalLocationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def rental_location_params
-      params.require(:rental_location).permit(:first_name, :last_name, :address_1, :address_2, :city, :state, :zip)
+      params.require(:rental_location).permit(:first_name, :last_name, :address_1,
+                                              :address_2, :city, :state, :zip, :phone)
     end
 end
