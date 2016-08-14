ae8f46c04f4637538eba515b2a4e9f64e377b869
Stephen Crespo <screspo988@gmail.com>
2016-08-14 12:31:04 -0400

    carousel captions

--- app/controllers/carousel_images_controller.rb
+++ app/controllers/carousel_images_controller.rb
@@ -62,12 +62,10 @@ class CarouselImagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_carousel_image
       @carousel_image = CarouselImage.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def carousel_image_params
       params.require(:carousel_image).permit(:name, :caption, :photo)
     end
